154e909ced61506c1f77a65d390217498db63df7
Bekhongbikcode <tammtse161087@fpt.edu.vn>
2023-03-19 17:50:34 +0700

    upload about, news page

---
+++ src/Component/More/Header.js
@@ -0,0 +1,14 @@
+import M from 'materialize-css'
+import 'materialize-css/dist/css/materialize.min.css'
+import React from 'react'
+import Footer from './Footer';
+import './More.css'
+
+function Header() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default Header;
